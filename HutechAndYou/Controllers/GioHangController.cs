1dfcefca36b4d332d1df978039c38199d241a8e9
tuan <myduyen98hcm@gmail.com>
2020-06-14 13:49:02 +0700

    tao giohang controller and home controller

---
+++ HutechAndYou/Controllers/GioHangController.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+using System.Web.Mvc;
+using HutechAndYou.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HutechAndYou.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbQLBLaptopDataContext data = new dbQLBLaptopDataContext();
+        //Giỏ Hàng      
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lGioHang = Session["GioHang"] as List<GioHang>;
+            if (lGioHang == null)
+            {
+                lGioHang = new List<GioHang>();
+                Session["GioHang"] = lGioHang;
+            }
+            return lGioHang;
+        }
+        public ActionResult ThemGioHang(int iMaSP, string strURL)
+        {
+            List<GioHang> lGioHang = LayGioHang();
+            GioHang sanpham = lGioHang.Find(n => n.iMaSP == iMaSP);
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(iMaSP);
+                lGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return Redirect(strURL);
+            }
+
+        }
+        //Tính Số Lượng
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lGioHang = Session["GioHang"] as List<GioHang>;
+            if (lGioHang != null)
+            {
+                iTongSoLuong = lGioHang.Sum(n => n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+        //Tính Tổng Số Tiền
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<GioHang> lGioHang = Session["GioHang"] as List<GioHang>;
+            if (lGioHang != null)
+            {
+                iTongTien = lGioHang.Sum(n => n.dThanhTien);
+            }
+            return iTongTien;
+        }
+        //Số dư
+        private Double DuTien()
+        {
+            Double DT;
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+            DT = double.Parse(kh2.SoTienCo.ToString());
+            return dSoDu;
+
+        }
+        public Double dSoDu { set; get; }
+        private double SoDu()
+        {
+            double iTongTien = 0;
+            double iSoDu = 0;
+            List<GioHang> lGioHang = Session["GioHang"] as List<GioHang>;
+            if (lGioHang != null)
+            {
+                iTongTien = lGioHang.Sum(n => n.dThanhTien);
+                KhachHang kh = (KhachHang)Session["TaiKhoan"];
+                KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+                dSoDu = double.Parse(kh2.SoTienCo.ToString());
+                iSoDu = dSoDu - iTongTien;
+            }
+            return iSoDu;
+        }
+
+        //Giỏ hàng
+        public ActionResult GioHangCuaBan()
+        {
+            List<GioHang> lGioHang = LayGioHang();
+            if (lGioHang.Count == 0)
+            {
+                return RedirectToAction("MuaHang","Home");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lGioHang);
+        }
+
+        //Hiện thị trên thanh công cụ
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return PartialView();
+        }
+
+        //Xóa giỏ hàng
+        public ActionResult XoaGioHang(int iMaSP)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.iMaSP == iMaSP);
+                return RedirectToAction("GioHangCuaBan");
+            }
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("GioHangCuaBan");
+        }
+
+        //Cập nhật số lượng
+        public ActionResult CapNhatSoLuong (int iMaSP, FormCollection f)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            if(sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(f["cnsoluong"].ToString());
+            }
+            return RedirectToAction("GioHangCuaBan");
+        }
+
+        //Xóa tất cả
+        public ActionResult XoaHetGioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        //Đặt hàng
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            List<GioHang> lstGioHang = LayGioHang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.SoDu = SoDu();
+            ViewBag.DT = DuTien();
+            ViewBag.USD = Int32.Parse(TongTien().ToString())/23200;
+            return View(lstGioHang);
+        }
+
+        //Xác nhận đặt hàng
+        [HttpPost]
+        public ActionResult DatHang(FormCollection collection)
+        {
+            DonDatHang ddh = new DonDatHang();
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+            List<GioHang> gh = LayGioHang();
+            var cbb = collection["cbb"];
+            ddh.IDKH = kh.IDKH;
+            var ttnh = collection["ttnh"];
+            ddh.NgayDat = DateTime.Now;
+            ddh.TinhTrang = 1;
+            ViewBag.usd = int.Parse(TongTien().ToString());
+            double sd = double.Parse(SoDu().ToString());           
+                if (cbb == "1")
+                {
+                if (sd >= 0)
+                {
+                    kh2.SoTienCo = int.Parse(SoDu().ToString());
+                        ddh.PhuongThuc = 1;
+                        ddh.DaThanhToan = int.Parse(TongTien().ToString()); ;
+                        data.DonDatHangs.InsertOnSubmit(ddh);
+                        data.SubmitChanges();
+                        //them chi tiet gio hang
+                        foreach (var laptop in gh)
+                        {
+                            ChiTietDDH ctdh = new ChiTietDDH();
+                            ctdh.MaDH = ddh.MaDH;
+                            ctdh.MaSP = laptop.iMaSP;
+                            ctdh.Soluong = laptop.iSoLuong;
+                            ctdh.Dongia = (decimal)laptop.dDonGia;
+                            UpdateModel(kh2);
+                            data.ChiTietDDHs.InsertOnSubmit(ctdh);
+                        }
+                    data.SubmitChanges();
+                    Session["Giohang"] = null;
+                    return RedirectToAction("XacNhanDonHang", "Giohang");
+                }
+                else
+                {
+                    ViewData["Loi"] = "tài khoản quý khách không đủ?";
+                }
+                }
+                else if (cbb == "2")
+                {
+                        ddh.PhuongThuc = 2;
+                        ddh.DaThanhToan = int.Parse(TongTien().ToString()); ;
+                        data.DonDatHangs.InsertOnSubmit(ddh);
+                        data.SubmitChanges();
+                        //them chi tiet gio hang
+                        foreach (var laptop in gh)
+                        {
+                            ChiTietDDH ctdh = new ChiTietDDH();
+                            ctdh.MaDH = ddh.MaDH;
+                            ctdh.MaSP = laptop.iMaSP;
+                            ctdh.Soluong = laptop.iSoLuong;
+                            ctdh.Dongia = (decimal)laptop.dDonGia;
+                            data.ChiTietDDHs.InsertOnSubmit(ctdh);
+                        }
+                        data.SubmitChanges();
+                        Session["Giohang"] = null;
+                        return RedirectToAction("XacNhanDonHang", "Giohang");
+                }
+                else if (cbb == "3")
+                {
+                    ddh.PhuongThuc = 3;
+                    ddh.DaThanhToan = int.Parse(TongTien().ToString()); ;
+                    data.DonDatHangs.InsertOnSubmit(ddh);
+                    data.SubmitChanges();
+                    //them chi tiet gio hang
+                    foreach (var laptop in gh)
+                    {
+                        ChiTietDDH ctdh = new ChiTietDDH();
+                        ctdh.MaDH = ddh.MaDH;
+                        ctdh.MaSP = laptop.iMaSP;
+                        ctdh.Soluong = laptop.iSoLuong;
+                        ctdh.Dongia = (decimal)laptop.dDonGia;
+                        data.ChiTietDDHs.InsertOnSubmit(ctdh);
+                    }
+                    data.SubmitChanges();
+                    Session["Giohang"] = null;
+                    return RedirectToAction("XacNhanDonHang", "Giohang");
+                }
+            
+            return this.DatHang();
+        }
+        public ActionResult XacNhanDonHang()
+        {
+            return View();
+        }
+
+        //Quản Lý đơn hàng
+        public ActionResult QuanLyDonHang()
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+        // Đơn hàng chờ xác nhận
+        // 1 Chờ xác nhận
+        // 2 Đã xác nhận
+        // 3 Đang giao hàng
+        // 4 đã nhận
+        // 5 Đã hủy
+        public ActionResult ListDonHang(int? page, int giaidoan)
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            int ID1 = kh.IDKH;
+            int pageSize = 10;
+            int pageNum = (page ?? 1);
+            var donhang = from s in data.DonDatHangs
+                          where s.IDKH == ID1 && s.TinhTrang == giaidoan
+                          select s;
+            ViewBag.ListDonHangTotal = donhang.Count();
+            return View(donhang.ToPagedList(pageNum, pageSize));
+        }
+
+        //Chi tiết đơn hàng
+        public ActionResult ChiTietDonHang(int ID, int? page)
+        {
+            int pageSize = 10;
+            int pageNum = (page ?? 1);
+            var CTDDH = from s in data.ChiTietDDHs
+                        where s.MaDH == ID
+                        select s;
+            var DonHang = (from s in data.DonDatHangs
+                        where s.MaDH == ID
+                        select s).FirstOrDefault();
+            ViewBag.MaDonHang = MD5Bam(DonHang.MaDH.ToString());
+            ViewBag.ThanhToan = DonHang.DaThanhToan;
+            ViewBag.NgayDat = DonHang.NgayDat;
+            ViewBag.TinhTrang = DonHang.TinhTrang;
+            ViewBag.PhuongThuc = DonHang.PhuongThuc;
+            return View(CTDDH.ToPagedList(pageNum, pageSize));
+        }
+
+        public static string MD5Bam(string text)
+        {
+            MD5 mh = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes("Chuỗi cần mã hóa");
+            byte[] hash = mh.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        // Xóa sản phẩm
+        [HttpGet]
+        public ActionResult XoaSanPham(int id)
+        {
+            DonDatHang ddh = data.DonDatHangs.SingleOrDefault(n => n.MaDH == id);
+            ViewBag.ddh = MD5Bam(id.ToString());
+            //ChiTietDDH ctdh = data.ChiTietDDHs.SingleOrDefault(n => n.MaDH == id);
+            if (ddh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ddh);
+        }
+        [HttpPost, ActionName("xoasanpham")]
+        public ActionResult XNXoaSanPham(int id)
+        {
+            DonDatHang ddh = data.DonDatHangs.SingleOrDefault(n => n.MaDH == id);
+            if(ddh.TinhTrang != 1)
+            {
+                //ChiTietDDH ctdh = data.ChiTietDDHs.SingleOrDefault(n => n.MaDH == id);
+                KhachHang kh = (KhachHang)Session["TaiKhoan"];
+                KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+                var cbb = ddh.PhuongThuc;
+                ddh.TinhTrang = -1;
+                double ngu = double.Parse(kh2.SoTienCo.ToString());
+                double ngu2 = double.Parse(ddh.DaThanhToan.ToString());
+                double ngu3 = ngu + ngu2;
+                if (cbb == 1)
+                {
+                    kh2.SoTienCo = int.Parse(ngu3.ToString());
+                }
+                UpdateModel(kh2);
+                UpdateModel(ddh);
+                data.SubmitChanges();
+            }
+            else
+            {
+                return RedirectToAction("XoaSanPhamLoi", "GioHang", routeValues: new { id = ddh.MaDH });
+            }
+            return RedirectToAction("QuanLyDonHang", "GioHang");
+        }
+        public ActionResult XoaSanPhamLoi(int id)
+        {
+            DonDatHang ddh = data.DonDatHangs.SingleOrDefault(n => n.MaDH == id);
+            ChiTietDDH ctdh = data.ChiTietDDHs.SingleOrDefault(n => n.MaDH == id);
+            ViewBag.ddh = ddh.MaDH;
+            ViewBag.ctdh = ctdh.MaDH;
+            if (ddh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ddh);
+        }
+
+        // chitiettaikhoan
+        [HttpGet]
+        public ActionResult ChiTietTaiKhoan()
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("Index", "Login");              
+            }
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+            ViewBag.dn = kh.HoTen;
+            double sd = double.Parse(kh2.SoTienCo.ToString());
+            ViewBag.dc2 = kh2.DiaChi;
+            ViewBag.ht = kh2.HoTen;
+            ViewBag.DT = int.Parse(sd.ToString());
+            ViewBag.em = kh2.Email;
+            ViewBag.sdt = kh2.DienThoai;
+            return View();
+        }
+
+        //Xác nhận đặt hàng
+        [HttpPost]
+        public ActionResult ChiTietTaiKhoan(FormCollection collection)
+        {
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+            var ht = collection["hoten"];
+            var dc = collection["diachi"];
+            if (string.IsNullOrEmpty(ht))
+            {
+                ViewData["loi"] = "mời nhập họ tên";
+            }
+            if (string.IsNullOrEmpty(dc))
+            {
+                ViewData["loi"] = "mời nhập dia chi";
+            }
+            else
+            {
+                if (dc.Length!=0 && ht.Length!=0)
+                {
+                    kh2.DiaChi = dc;
+                    kh2.HoTen = ht;
+                    UpdateModel(kh2);
+                }
+                data.SubmitChanges();
+            }
+            return this.ChiTietTaiKhoan();
+        }
+
+        public ActionResult Hientaikhoan()
+        {
+            if (Session["TaiKhoan"] != null)
+            {
+                KhachHang kh = (KhachHang)Session["TaiKhoan"];
+                KhachHang kh2 = data.KhachHangs.SingleOrDefault(n => n.IDKH == kh.IDKH);
+                ViewBag.dn = kh2.HoTen;
+            }
+            else
+            {
+                ViewBag.dn = "Đăng nhập";
+            }
+            return PartialView();
+        }
+
+        public ActionResult ThanhToanOnline()
+        {
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            List<GioHang> gh = LayGioHang();
+            return View();
+        }
+    }
+}
---
+++ HutechAndYou/Controllers/HomeController.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HutechAndYou.Models;
+using PagedList;
+using PagedList.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace HutechAndYou.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbQLBLaptopDataContext data = new dbQLBLaptopDataContext();
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 3;
+            int pageNum = (page ?? 1);
+            var tintuc = from s in data.Tintucs
+                         select s;
+            return View(tintuc.ToPagedList(pageNum, pageSize));
+        }
+
+        //View tin tức
+        public ActionResult TinTucAdmin(int ID)
+        {
+            var Tintuc = from s in data.Tintucs
+                          where s.MaTinTuc == ID
+                          select s;
+            return View(Tintuc.Single());
+        }
+
+        public ActionResult AutoComName()
+        {
+            var spName = (from s in data.SanPhams
+                          select s.TenSP).ToList();
+            return Json(spName, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult BoLoc()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        //Liên hệ
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(FormCollection collection, Lienhe lh, HttpPostedFileBase fileUpload)
+        {
+            var ten = collection["Name"];
+            var email = collection["Email"];
+            var noidung = collection["Message"];
+            if (string.IsNullOrEmpty(ten))
+            {
+                ViewData["loi1"] = "nhập tên của bạn";
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                ViewData["loi2"] = "nhập email của bạn";
+            }
+            else if (string.IsNullOrEmpty(noidung))
+            {
+                ViewData["loi3"] = "nhập nội dung bạn muốn nói với chúng tôi";
+            }
+            else
+            {
+                lh.TenKH = ten;
+                lh.Emai = email;
+                lh.NoiDung = noidung;
+                data.Lienhes.InsertOnSubmit(lh);
+                data.SubmitChanges();
+                return RedirectToAction("ContactTC");
+            }
+            return this.Contact();
+        }
+
+        //Liên hệ thành công
+        public ActionResult ContactTC()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Guarantee()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult KhuTraiNghiem()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //Sắp xếp giảm dần theo ngày cập nhật:
+        public List<SanPham> LaptopMoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
+        //Mua hàng
+        public ActionResult MuaHang (int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var laptopmoi = LaptopMoi(100000);
+            ViewBag.spLocCount = laptopmoi.Count();
+            return View(laptopmoi.ToPagedList(pageNum, pageSize));
+
+        }
+
+        public ActionResult MuaHangLoc(int? page, int hang, string gia, string cpu)
+        {
+            var cpus = cpu;
+            var giaf = gia;
+            if(cpu == "-1")
+            {
+                cpus = "i";
+            }
+            var laptopmoi = (from s in data.SanPhams
+                            where s.CPU.Contains(cpus)
+                            orderby s.NgayCapNhat descending
+                            select s).ToList();
+            if(hang != -1)
+            {
+                if (gia != "-1")
+                {
+                    var giaArrry = gia.Split('a');
+                    var batdau = giaArrry[0];
+                    var ketthuc = giaArrry[1];
+                    laptopmoi = (from s in data.SanPhams
+                                 where s.MaHang == hang && s.CPU.Contains(cpus)
+                                 && s.GiaBan >= Convert.ToInt64(batdau) && s.GiaBan <= Convert.ToInt64(ketthuc)
+                                 orderby s.NgayCapNhat descending
+                                 select s).ToList();
+                }
+                else
+                {
+                    laptopmoi = (from s in data.SanPhams
+                                 where s.MaHang == hang && s.CPU.Contains(cpus)
+                                 orderby s.NgayCapNhat descending
+                                 select s).ToList();
+                }
+            }
+            else if (hang == -1 && gia != "-1")
+            {
+                var giaArrry = gia.Split('a');
+                var batdau = giaArrry[0];
+                var ketthuc = giaArrry[1];
+                laptopmoi = (from s in data.SanPhams
+                             where s.CPU.Contains(cpus)
+                             && s.GiaBan >= Convert.ToInt64(batdau) && s.GiaBan <= Convert.ToInt64(ketthuc)
+                             orderby s.NgayCapNhat descending
+                             select s).ToList();
+            }
+            
+            ViewBag.giaf = giaf;
+            ViewBag.cpuf = cpu;
+            ViewBag.hangf = hang;
+            ViewBag.spLocCount = laptopmoi.Count();
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            return View(laptopmoi.ToPagedList(pageNum, pageSize));
+        }
+
+        public static float giamgia(float giagiam)
+        {
+            float a = 1;
+            if (giagiam != 0)
+            {
+                a = giagiam/100;
+            }
+            return a;
+        }
+
+        //Tìm kiêm
+        public ActionResult Search(string searchTerm,int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var books = from b in data.SanPhams select b;
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                books = data.SanPhams.Where(b => b.Hang.TenHang.Contains(searchTerm));
+            }
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                books = data.SanPhams.Where(b => b.TenSP.Contains(searchTerm));
+            }
+            ViewBag.SearchTerm = searchTerm;
+            ViewBag.spLocCount = books.Count();
+            return View(books.ToPagedList(pageNum, pageSize));
+        }
+
+        //Chi Tiết
+        public ActionResult ChiTiet(int ID)
+        {
+            var SanPham = from s in data.SanPhams
+                          where s.MaSP == ID
+                          select s;
+            return View(SanPham.Single());
+        }
+
+
+        //Chi Tiết Admin
+        public ActionResult ChiTietAdmin(int ID)
+        {
+            var SanPham = from s in data.SanPhams
+                          where s.MaSP == ID
+                          select s;
+            return View(SanPham.Single());
+        }
+
+        //Đăng ký thành công
+        public ActionResult DangKyTC()
+        {
+            return View();
+        }
+
+        //Đăng nhập thành công
+        public ActionResult DangNhapTC()
+        {
+            return View();
+        }
+
+        //Hãng Sản Xuất:
+        public ActionResult HangSanXuat()
+        {
+            var hangsanxuat = from hangsx in data.Hangs select hangsx;
+            return PartialView(hangsanxuat);
+        }
+
+        //Hãng Sản Xuất v2:
+        public ActionResult HangSanXuatV2()
+        {
+            var hangsanxuat = from hangsx in data.Hangs select hangsx;
+            return View(hangsanxuat);
+        }
+
+        //Sản Phẩm theo hãng:
+        public ActionResult SPTheoHang(int ID, int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams where s.MaHang == ID select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia 0-10tr:
+        public ActionResult DEN10(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan <= 10000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia 10-15tr:
+        public ActionResult DEN15(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan > 10000000 && s.GiaBan <= 15000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia 15-20tr:
+        public ActionResult DEN20(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan > 15000000 && s.GiaBan <= 20000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia 20-25tr:
+        public ActionResult DEN25(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan > 20000000 && s.GiaBan <= 25000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia 25-30tr:
+        public ActionResult DEN30(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan > 25000000 && s.GiaBan <= 30000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+
+        //Gia tren 30tr:
+        public ActionResult TREN30(int? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var SanPham = from s in data.SanPhams
+                          where s.GiaBan > 30000000
+                          select s;
+            return View(SanPham.ToPagedList(pageNum, pageSize));
+
+        }
+    }
+}
