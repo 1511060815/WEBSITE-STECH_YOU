47ba420a8c8b54f8820daa24110101806ff14199
tuan <myduyen98hcm@gmail.com>
2020-06-14 14:19:21 +0700

    chinh sua models

--- HutechAndYou/Models/Logger.cs
+++ HutechAndYou/Models/Logger.cs
@@ -11,7 +11,7 @@ namespace HutechAndYou.Models
         public static void Log(String lines)
         {
             // Write the string to a file.append mode is enabled so that the log
-            // lines get appended to test.txt than wiping content and writing the log
+            // lines get appended to test.txt than wiping content and writing the logss
             try
             {
                 System.IO.StreamWriter file = new
